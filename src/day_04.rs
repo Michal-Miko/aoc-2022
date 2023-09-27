08ee2a1698aa367bd4435b939ec9362952324432
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2023-09-27 22:58:42 +0200

    Day04

--- src/day_04.rs
+++ src/day_04.rs
@@ -2,8 +2,53 @@ use std::path::PathBuf;
 
 use crate::BoxedError;
 use aoc_framework::{traits::*, AocSolution, AocStringIter, AocTask};
+use color_eyre::{eyre::eyre, Report};
+use itertools::Itertools;
+use lazy_static::lazy_static;
+use regex::Regex;
 
 pub struct Day04;
+
+lazy_static! {
+    static ref LINE_REGEX: Regex = Regex::new(r#"(\d+)-(\d+),(\d+)-(\d+)"#).expect("Invalid regex");
+}
+
+#[derive(Debug)]
+pub struct Pair {
+    left: (i32, i32),
+    right: (i32, i32),
+}
+
+impl Pair {
+    fn full_overlap(&self) -> bool {
+        let left_in_right = self.left.0 >= self.right.0 && self.left.1 <= self.right.1;
+        let right_in_left = self.right.0 >= self.left.0 && self.right.1 <= self.left.1;
+        left_in_right || right_in_left
+    }
+
+    fn partial_overlap(&self) -> bool {
+        let left_overlap = self.left.0 <= self.right.1 && self.left.1 >= self.right.0;
+        let right_overlap = self.right.0 <= self.left.1 && self.right.1 >= self.left.0;
+        left_overlap || right_overlap
+    }
+}
+
+impl TryFrom<String> for Pair {
+    type Error = Report;
+
+    fn try_from(value: String) -> Result<Self, Report> {
+        let matches: [&str; 4] = LINE_REGEX
+            .captures(&value)
+            .ok_or(eyre!("Invalid pair: {value}"))?
+            .extract()
+            .1;
+
+        Ok(Self {
+            left: (matches[0].parse()?, matches[1].parse()?),
+            right: (matches[2].parse()?, matches[3].parse()?),
+        })
+    }
+}
 
 impl AocTask for Day04 {
     fn directory(&self) -> PathBuf {
@@ -11,6 +56,16 @@ impl AocTask for Day04 {
     }
 
     fn solution(&self, input: AocStringIter, phase: usize) -> Result<AocSolution, BoxedError> {
-        input.solved()
+        let filter = match phase {
+            1 => Pair::full_overlap,
+            2 => Pair::partial_overlap,
+            _ => todo!(),
+        };
+
+        input
+            .map(Pair::try_from)
+            .filter_ok(filter)
+            .process_results(|results| results.count())?
+            .solved()
     }
 }
