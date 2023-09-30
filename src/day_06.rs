e97117baf515bf7c81c109a9ef0c57e28634c14d
Michał Mikołajczyk <38507424+Michal-Miko@users.noreply.github.com>
2023-09-30 16:41:10 +0200

    Days 02,06

--- src/day_06.rs
+++ src/day_06.rs
@@ -2,6 +2,8 @@ use std::path::PathBuf;
 
 use crate::BoxedError;
 use aoc_framework::{traits::*, AocSolution, AocStringIter, AocTask};
+use color_eyre::eyre::eyre;
+use itertools::Itertools;
 
 pub struct Day06;
 
@@ -10,7 +12,24 @@ impl AocTask for Day06 {
         "tasks/day_06".into()
     }
 
-    fn solution(&self, input: AocStringIter, phase: usize) -> Result<AocSolution, BoxedError> {
-        input.solved()
+    fn solution(&self, mut input: AocStringIter, phase: usize) -> Result<AocSolution, BoxedError> {
+        let marker_window_size = match phase {
+            1 => 4,
+            2 => 14,
+            _ => todo!(),
+        };
+
+        let datastream: Vec<char> = input
+            .next()
+            .ok_or(eyre!("Missing input"))?
+            .chars()
+            .collect();
+
+        let (marker_pos, _) = datastream
+            .windows(marker_window_size)
+            .find_position(|window| window.iter().all_unique())
+            .ok_or(eyre!("No unique window found"))?;
+
+        (marker_pos + marker_window_size).solved()
     }
 }
